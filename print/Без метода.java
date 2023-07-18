7173d213ff0b02915244536962a748e501ef9418
javaledi <139841183+javaledi@users.noreply.github.com>
2023-07-18 12:18:21 +0700

    Add files via upload

---
+++ print/Без метода.java
@@ -0,0 +1,13 @@
+class Solution
+{
+   public static void main(String[] args)
+   {
+     System.out.print("Wi-");
+     System.out.println("Fi");
+     System.out.print("Wi-");
+     System.out.println("Fi");
+
+     System.out.print("Wi-");
+     System.out.println("Fi");
+   }
+}
