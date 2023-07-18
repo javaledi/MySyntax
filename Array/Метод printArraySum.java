0763396fe8f8acb94c006fab80df7ec61eddd72a
javaledi <139841183+javaledi@users.noreply.github.com>
2023-07-18 13:22:00 +0700

    Add files via upload

---
+++ Array/Метод printArraySum.java
@@ -0,0 +1,18 @@
+class Solution
+{
+   public static void printArraySum(int[] data)
+   {
+     int sum = 0;
+     for (int i = 0; i < data.length; i++)
+       sum = sum + data[i];
+     System.out.println(sum);
+   }
+   
+   public static void main(String[] args)
+   {
+     int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30};
+     printArraySum(months);
+   }
+}
+
+// Output: 273
