c30521c7d1047fde3b57a1dfa445f2392acbbd1e
javaledi <139841183+javaledi@users.noreply.github.com>
2023-07-19 21:19:49 +0700

    Delete Оператор преобразования типа.java

--- Variables/Primitive types/Приведение типов/Сужение типов/Оператор преобразования типа.java
+++
@@ -1,9 +0,0 @@
-long a = 1;
-int b = (int) a;
-short c = (short) b;
-byte d = (byte) c;
-
-long a = 1000000; // a == 1000000
-int b = (int) a; // b == 1000000
-short c = (short) b; // c == 16960
-byte d = (byte) c; // d == 64
