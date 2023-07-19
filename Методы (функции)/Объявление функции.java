aff5bdc6be5a214e5404f11e8eb87386695497ec
javaledi <139841183+javaledi@users.noreply.github.com>
2023-07-19 16:58:47 +0700

    Add files via upload

---
+++ Методы (функции)/Объявление функции.java
@@ -0,0 +1,28 @@
+int doWork() // Функция/метод правильно объявлена
+
+// !Function DoWork()
+ 
+// function DoWork()
+ 
+// Procedure DoWork()
+ 
+
+class Solution
+{
+   public static void main(String[] args)
+   {
+     printWiFi10Times(); // Вызываем метод printWiFi10Times
+   }
+
+   public static void printWiFi10Times() // Объявляем метод printWiFi10Times
+   {
+     for (int i = 0; i < 10; i++)
+       printWiFi(); // Вызываем метод printWiFi10Times 10 раз в цикле
+   }
+
+   public static void printWiFi()
+   {
+     System.out.print("Wi-"); // Выводим на экран текст:
+     System.out.println("Fi"); // Wi-Fi
+   }
+}
